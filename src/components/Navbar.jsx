2f63dfbb582b669c9b6458ee7b84cf65826aa3fa
unknown <ashuisalive@gmail.com>
2021-09-05 16:01:29 +0530

    bootstrap added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+            
+        </nav>
+    )
+}
+
+export default Navbar;
